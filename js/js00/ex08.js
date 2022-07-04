fe1ae753442fc4f09a4e9e10a0bf8b6b5a5bd776
MyNNeiei <kullananzaza@gmail.com>
2022-07-04 20:53:18 +0700

    js3

---
+++ js/js00/ex08.js
@@ -0,0 +1,3 @@
+let a = 365
+console.log('Enter days:', a);
+console.log(a ,"days = ", a/365 ,"year/s,", a%365 ,"week/s and" , (a%365)%365 ,"day/s");
---
+++ js/js00/ex10.js
@@ -0,0 +1,4 @@
+let a = 25
+const b = Math.sqrt(a);
+console.log('Enter any number:', a);
+console.log('Square root of 25 =', b)
