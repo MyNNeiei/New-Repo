b2fd8c2ff35a0efd66108cfddefb14c66b0de114
MyNNeiei <kullananzaza@gmail.com>
2022-08-05 11:28:15 +0700

    js14

--- js/js01/ex05.js
+++ js/js01/ex05.js
@@ -1,3 +1,9 @@
 var redlineSync = require('readline-sync')
 
-let cost = redlineSync.question('Input cost price: ');
+let year = redlineSync.question('Input year: ');
+if(year%100 !== 0 && year%4 == 0){
+    console.log( year ,'is leap year')
+}
+else{
+    console.log(year ,'is not leap year')
+}
